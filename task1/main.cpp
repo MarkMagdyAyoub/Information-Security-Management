6f7ccffb48de11519b56e95de4d19979cd086f53
Mark Magdy <markgeforce4080@gmail.com>
2025-03-02 16:22:37 +0200

    more intuative brute force solution + avoid race condition + terminate other threads once a password match is found

--- task1/main.cpp
+++ task1/main.cpp
@@ -3,10 +3,18 @@
 #include <fstream>
 #include <string>
 #include <thread>
+#include <functional>
+#include <atomic>
+
 #define PASSWORD7 "./dictionary/7-more-passwords.txt"
 #define PASSWORD8 "./dictionary/8-more-passwords.txt"
-#define REAL_PASSWORD "Leonard1" // assume it is given from the database
+#define REAL_PASSWORD "abc" // assume it is given from the database
+
 using namespace std;
+
+// i used this flag to early break all threads if i found the answer in one thread
+// the shared resource should be atomic to avoid race condition
+atomic<bool> passwordFound(false); // shared variable(flag)
 
 bool check_password(const string &filePath, const string &password)
 {
@@ -17,9 +25,12 @@ bool check_password(const string &filePath, const string &password)
   string savedPassword;
   while (getline(fin, savedPassword))
   {
+    if (passwordFound.load())
+      return false;
     if (password == savedPassword)
     {
       fin.close();
+      passwordFound.store(true);
       return true;
     }
   }
@@ -27,59 +38,55 @@ bool check_password(const string &filePath, const string &password)
   return false;
 }
 
-bool brute_force()
+bool brute_force(int length)
 {
-  char characters[] = {
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-  int size = sizeof(characters) / sizeof(characters[0]);
-
-  for (int i = 0; i < size; i++)
+  string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  function<bool(string word, int n)> generate = [&](string word, int n)
   {
-    for (int j = 0; j < size; j++)
+    if (passwordFound.load())
+      return false;
+    if (n == length)
     {
-      for (int k = 0; k < size; k++)
+      if (word == REAL_PASSWORD)
       {
-        for (int l = 0; l < size; l++)
-        {
-          for (int m = 0; m < size; m++)
-          {
-            string result = string(1, characters[i]) + characters[j] + characters[k] + characters[l] + characters[m];
-            if (result == REAL_PASSWORD)
-              return true;
-          }
-        }
+        passwordFound.store(true);
+        return true;
       }
+      return false;
     }
-  }
-  return false;
+    for (char c : chars)
+      if (generate(word + c, n + 1))
+        return true;
+    return false;
+  };
+  return generate("", 0);
 }
 
 /**
  * Creates two threads:
- * - One for searching in the dictionary files
- * - One for brute force
+ * [1] One for searching in the dictionary files
+ * [2] One for brute force
  */
 
 int main()
 {
-  // dictionary attack
   string username;
   bool searchFound = false, bfFound = false;
 
   getline(cin, username);
 
+  // dictionary attack
   thread search(
       [&]()
       {
         searchFound = check_password(PASSWORD7, REAL_PASSWORD) || check_password(PASSWORD8, REAL_PASSWORD);
       });
 
+  // brute force
   thread bf(
       [&]()
       {
-        bfFound = brute_force();
+        bfFound = brute_force(5); // 52^5 --> 380,204,032
       });
 
   search.join();
